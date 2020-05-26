f23369490c884686a1ddda28e222dbfaa7d1999e
AgusFerro <agusferroig@gmail.com>
2020-05-26 18:20:53 -0300

    TP3 10%

---
+++ TP3/Ferro.Agustin.2C.TP3/ClasesInstanciables/Profesor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClasesAbstractas;
+using Excepciones;
+
+namespace ClasesInstanciables
+{
+    public sealed class Profesor : Universitario
+    {
+    }
+}
